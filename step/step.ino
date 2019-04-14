c07acb31f570f9d9d403befb209b0cce89c1e321
WaterTian <changewater@qq.com>
2019-04-14 18:13:46 +0800

    save complete led v1

--- step/step.ino
+++ step/step.ino
@@ -1,82 +1,117 @@
+#include <FastLED.h>
 
-#include <FastLED.h>          // 此示例程序需要使用FastLED库
- 
-#define NUM_LEDS 144             // LED灯珠数量
-//#define DATA_PIN 7              // Arduino输出控制信号引脚
-//#define LED_TYPE WS2811         // LED灯带型号
-//#define COLOR_ORDER GRB         // RGB灯珠中红色、绿色、蓝色LED的排列顺序
-//uint8_t max_bright = 128;       // LED亮度控制变量，可使用数值为 0 ～ 255， 数值越大则光带亮度越高
+#define NUM_LEDS 144 //每条led数
+#define NUM_STEPS 5 //台阶数
+#define FRAMES_PER_SECOND  90  //FPS
 
-CRGB leds[3][NUM_LEDS];  // 建立光带leds
-//CRGB leds[NUM_LEDS];     // 建立光带leds       
+//CRGB leds[NUM_LEDS];     // 创建光带leds
+CRGB leds[NUM_STEPS][NUM_LEDS];
 
+int aPins[NUM_STEPS] = {0, 2, 3}; //a口s
+
+float lifes[NUM_STEPS]; //0~1
+
+CRGBPalette16 currentPalette = CloudColors_p;
 
 void setup() {
-  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+  Serial.begin(9600);
   delay(2000);
 
+  //FastLED.addLeds<WS2812B, 7>(leds, NUM_LEDS); //型号,信号引脚,(已创建的光带,LED灯珠数量)
+  FastLED.setBrightness(64);// 设置光带亮度 0～255
 
-  FastLED.addLeds<WS2811, 4>(leds[0], NUM_LEDS);
-  FastLED.addLeds<WS2811, 7>(leds[1], NUM_LEDS);
-  FastLED.addLeds<WS2811, 11>(leds[2], NUM_LEDS);
 
-  
-//  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);  // 初始化光带 
-  
-  FastLED.setBrightness(32);                              // 设置光带亮度
+  FastLED.addLeds<WS2812B, 4>(leds[0], NUM_LEDS);
+  FastLED.addLeds<WS2812B, 7>(leds[1], NUM_LEDS);
+  FastLED.addLeds<WS2812B, 11>(leds[2], NUM_LEDS);
+
+
+
+
+  startClear();
+}
+void startClear() {
+  FastLED.clear();
+  for (int i = 0; i < NUM_STEPS; i++) {
+    fill_solid(leds[i], NUM_LEDS, CRGB::White);
+    FastLED.show();
+    delay(100);
+    fill_solid(leds[i], NUM_LEDS, CRGB::Black);
+    FastLED.show();
+  }
+}
+
+
+
+
+
+
+void FillPaletteColors(int id, uint8_t colorIndex)
+{
+  if (lifes[id] <= 0)
+  {
+    fill_solid(leds[id], NUM_LEDS, CRGB::Black);
+    return;
+  }
+
+
+  uint8_t brightness = 255 * lifes[id];
+
+  for ( int i = 0; i < NUM_LEDS; i++) {
+    leds[id][i] = ColorFromPalette( currentPalette, colorIndex, brightness, LINEARBLEND);
+    colorIndex += 3;
+  }
+
+  if (lifes[id] > 0.9)addGlitter(id, 60);
+
+
+}
+
+void addGlitter(int id, fract8 chanceOfGlitter)
+{
+  if ( random8() < chanceOfGlitter) {
+    leds[id][ random16(NUM_LEDS) ] += CRGB::White;
+  }
 }
 
 void loop() {
-  FastLED.clear();  
 
+  static uint8_t startIndex = 0;
+  startIndex = startIndex + 1; /* motion speed */
 
-
-
-
-    
-  //  for (int i = 0; i < NUM_LEDS; i++) {
-  //    leds[i - 3].setRGB(0, 0, 0);
-  //    leds[i - 2].setRGB(60, 60, 60);
-  //    leds[i - 1].setRGB(180, 180, 180);
-  //    leds[i].setRGB(255, 255, 255);
-  //    FastLED.show();
-  //    delay(1);
-  //  }
-  //  FastLED.clear();
-  
-  
-  
-
-  int a0 = analogRead(0);
-  int a1 = analogRead(2);
-  int a2 = analogRead(3);
 
   FastLED.clear();
 
-  if(a0<100)
-  {
-    fill_solid(leds[0], NUM_LEDS, CRGB::White);
-  }else{
-    fill_solid(leds[0], NUM_LEDS, CRGB::Black);
+  for (int i = 0; i < NUM_STEPS; i++) {
+    int _a = analogRead(aPins[i]);
+
+    
+    if (_a < 100) {
+      lifes[i] = lifes[i] < 1 ? lifes[i] += 0.25 : 1;
+
+      FillPaletteColors(i, startIndex);
+    }
+    else {
+      lifes[i] = lifes[i] > 0 ? lifes[i] -= 0.05 : 0;
+
+      FillPaletteColors(i, startIndex);
+
+    }
+
   }
 
 
-  if(a1<100)
-  {
-    fill_solid(leds[1], NUM_LEDS, CRGB::White);
-  }else{
-    fill_solid(leds[1], NUM_LEDS, CRGB::Black);
-  }
 
+  //  Serial.println("____");
+  //  Serial.print(lifes[2]);
 
-  if(a2<100)
-  {
-    fill_solid(leds[2], NUM_LEDS, CRGB::White);
-  }else{
-    fill_solid(leds[2], NUM_LEDS, CRGB::Black);
-  }
-  
   FastLED.show();
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
+
+
+
+
+
 
 
 }
